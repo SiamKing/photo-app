3ccbde4bf78d87a2d60734ee164627ffff5e4598
SiamKing <thesiameseelectric@gmail.com>
2019-10-01 11:19:23 -0700

    Updated create method because of errors

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,39 +7,33 @@ class RegistrationsController < Devise::RegistrationsController
             resource.save
             yield resource if block_given?
             if resource.persisted?
-                @payment = Payment.new({    email: params["user"]["email"],
-                                            token: params[:payment]["token"],
-                                            user_id: resource.id })
+                @payment = Payment.new({ email: params[:user]["email"], 
+                token: params[:payment]["token"], user_id: resource.id })
 
-                flash[:error] = "please check registration errors" unless @payment.valid?
+                flash[:error] = "Please check registration errors" unless @payment.valid?
 
-                begin
-                    @payment.process_payment
-                    @payment.save
-                rescue Exception => e
-                    flash[:error] = e.message
+                @payment.process_payment
+                @payment.save
 
-                    resource.destroy
-                    puts 'Payment failed'
-                    render :new and return
+                if resource.active_for_authentication?
+                set_flash_message! :notice, :signed_up
+                sign_up(resource_name, resource)
+                respond_with resource, location: after_sign_up_path_for(resource)
+                else
+                set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+                expire_data_after_sign_in!
+                respond_with resource, location: after_inactive_sign_up_path_for(resource)
                 end
-
-                    if resource.active_for_authentication?
-                        set_flash_message! :notice, :signed_up
-                        sign_up(resource_name, resource)
-                        respond_with resource, location: after_sign_up_path_for(resource)
-                    else
-                        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
-                        expire_data_after_sign_in!
-                        respond_with resource, location: after_inactive_sign_up_path_for(resource)
-                    end
             else
-                byebug
                 clean_up_passwords resource
                 set_minimum_password_length
                 respond_with resource
             end
         end
+        rescue Exception => e
+            flash[:error] = e.message
+            puts "Payment failed"
+            render :new and return
     end
 
     protected
