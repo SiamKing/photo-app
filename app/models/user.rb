426b9603aeea584c423836ba253817bfbec278ad
SiamKing <thesiameseelectric@gmail.com>
2019-10-02 13:05:41 -0700

    Added functionality to upload photos and configured carrierwave to use aws s3

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
 
   has_one :payment
+  has_many :images
   accepts_nested_attributes_for :payment
 end
